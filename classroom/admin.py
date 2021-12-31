4ab8a59fe45b91ad18daa75e59d840e2ca50c6bf
TrinaPandit <trina.pandit@northsouth.edu>
2022-01-01 03:31:34 +0600

    deletefile

--- classroom/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User,Student,Teacher
-# Register your models here.
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Student)
-admin.site.register(Teacher)
-
