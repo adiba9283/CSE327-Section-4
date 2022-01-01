4f122e91389681d0f4368ef85c8846f1d2ad0cdd
TrinaPandit <trina.pandit@northsouth.edu>
2022-01-02 02:42:48 +0600

    Unit Testing

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import User, Teacher, Student
+from .views import user_login
+import json
 
-# Create your tests here.
+class TestLogin(TestCase):
+    
+      def setUp(self):
+        self.client = Client()
+
+      def test_user_login_status_code(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'template/login.html')
