bfe4651e64223405b0e582fbb7f304d497e99ec0
adiba9283 <tabassumadiba786@gmail.com>
2022-01-01 20:42:41 +0600

    unittesting attempt

--- classmanager/classroom/tests.py
+++ classmanager/classroom/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
+import unittest
+
+from .models import MessageToTeacher
+
 # Create your tests here.
+
+class TestMessageToTeacher(unittest.TestCase):
+
+    def __str__(self):
+        return self.message
+    
+    def test_urls(self):
+        pass
+
+    def test_views(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
+
