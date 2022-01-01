46008736588cb32fa4cc2a1d0fa79ccb54e88851
Nafisa Anjum Bristy <nafisa.anjum02@northsouth.edu>
2022-01-01 23:37:09 +0600

    Unit testing for studentslist function

--- classroom/tests.py
+++ classroom/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+import unittest
+from .models import StudentsInClass
 
 # Create your tests here.
+
+class TestStudentsInClass(unittest.TestCase):
+
+
+
+    def str(self):
+        return self.student.name
+
+    def test_urls(self):
+        pass
+
+    def test_views(self):
+        pass
+
+if name == 'main':
+    unittest.main()
+
