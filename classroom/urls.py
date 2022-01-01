085b481b742075a02e4bbae4b14179db8c7d5e3c
TrinaPandit <trina.pandit@northsouth.edu>
2022-01-01 19:15:55 +0600

    Create account, Login, view profile, edit profile, change password

--- classroom/urls.py
+++ classroom/urls.py
@@ -4,6 +4,19 @@ from classroom import views
 app_name = 'classroom'
 
 urlpatterns =[
+   
+    path('signup/',views.SignUp,name="signup"),
+    path('signup/student_signup/',views.StudentSignUp,name="StudentSignUp"),
+    path('signup/teacher_signup/',views.TeacherSignUp,name="TeacherSignUp"),
+    path('login/',views.user_login,name="login"),
+    path('logout/',views.user_logout,name="logout"),
+    path('student/<int:pk>/',views.StudentDetailView.as_view(),name="student_detail"),
+    path('teacher/<int:pk>/',views.TeacherDetailView.as_view(),name="teacher_detail"),
+    path('update/student/<int:pk>/',views.StudentUpdateView,name="student_update"),
+    path('update/teacher/<int:pk>/',views.TeacherUpdateView,name="teacher_update"),
+    path('change_password/',views.change_password,name="change_password"),
+
+   
     path('upload_assignment/',views.upload_assignment,name="upload_assignment"),
     path('class_assignment/',views.class_assignment,name="class_assignment"),
     path('assignment_list/',views.assignment_list,name="assignment_list"),
